b8b7e3d6db76de2c2b0887db6577b21108b23fc4
Ray Tseng <ray_tseng@trend.com.tw>
2014-11-29 01:34:28 +0800

    1. Add method for parse data from total record page. 2. modify .gitignore

--- python/parser.py
+++ python/parser.py
@@ -10,7 +10,14 @@ from urllib2 import urlopen,Request
 import urlparse
 import time  
 
+from collections import OrderedDict
+
+#from bson import json_util
 import json
+
+retr_text = lambda nodes, pipe='': pipe.join([''.join(i.itertext()) for i in nodes])
+retr_one = lambda nodes: nodes.text if len(nodes)==0 else nodes[0].text
+retr_list = lambda nodes : filter(None, [i.strip() for i in nodes.itertext()])
 
 
 amazon_url = 'http://www.sumo.or.jp/'
@@ -43,27 +50,110 @@ def add_into_done_list(key_string):
     global done_dict
     done_dict.append(key_string)
 
-    with open('output/done_lists.txt', 'a+') as f:
+    with open('output/done_lists.txt', 'w+') as f:
         f.write(key_string + '\n')
+
+def save_total_record_json(data):
+    ## save info into json
+    with open('output/docs.json', 'w+') as f:
+        doc_str = json.dumps(data)
+        f.write(doc_str)
 
 
 def parseHTML(data):
     tree = etree.HTML(data)
 
-
     # get east total record
     east_elements = tree.xpath('//*/div[@id="east"]/table[@class="main "]/tr')
-    #east_elements = tree.cssselect('div.east table.main')
+    # print len(east_elements)
 
-    print len(east_elements)
-    for x in east_elements:
-        print x
+
+    #totalPlayer = {}
+    totalPlayer = OrderedDict()
+
+    # get EAST player name, rank, and total record with competitor
+    for index in xrange(0, len(east_elements), 2):
+
+        singlePlayer = {}
+
+        playerInfo = east_elements[index].xpath('td[@class="player bBnone"]/div/dl')
+        recordInfo = east_elements[index].xpath('td/img')
+        competitorInfo = east_elements[index+1].xpath('td')
+
+
+        if len(playerInfo) == 1:
+            info = retr_list(playerInfo[0])
+            singlePlayer['rank'] = info[0]
+            singlePlayer['name'] = info[1]
+            singlePlayer['record'] = info[2]
+            # print info[0], info[1], info[2]
+
+        #print singlePlayer
+
+        # wbRecord = [i.get('alt') for i in recordInfo]
+        # 白丸unicode = u'\u767d\u4e38'
+        wbRecord = ['O' if i.get('alt') == u'\u767d\u4e38' else 'X' for i in recordInfo]
+        singlePlayer['win_lose_list'] = wbRecord
+        #print wbRecord
+
+
+        # get competitor info and order
+        comRecord = [i.text for i in competitorInfo]
+        singlePlayer['competitor_list'] = comRecord
+        #print comRecord
+
+
+        totalPlayer["E" + str(index/2 + 1)] = singlePlayer
+
 
 
     # get west total record
-    # west_element = tree.xpath('//*/div[@id="west"]')
+    west_elements = tree.xpath('//*/div[@id="west"]/table[@class="main "]/tr')
+    # print len(west_elements)
 
-    return True
+
+    # get WEST player name, rank, and total record with competitor
+    for index in xrange(0, len(west_elements), 2):
+
+        singlePlayer = {}
+
+        playerInfo = west_elements[index].xpath('td[@class="player bBnone"]/div/dl')
+        recordInfo = west_elements[index].xpath('td/img')
+        competitorInfo = west_elements[index+1].xpath('td')
+
+
+        if len(playerInfo) == 1:
+            info = retr_list(playerInfo[0])
+            singlePlayer['rank'] = info[0]
+            singlePlayer['name'] = info[1]
+            singlePlayer['record'] = info[2]
+            # print info[0], info[1], info[2]
+
+        #print singlePlayer
+
+        # 白丸unicode = u'\u767d\u4e38'
+        wbRecord = ['O' if i.get('alt') == u'\u767d\u4e38' else 'X' for i in recordInfo]
+        singlePlayer['win_lose_list'] = wbRecord
+        #print wbRecord
+
+
+        # get competitor info and order
+        comRecord = [i.text for i in competitorInfo]
+        singlePlayer['competitor_list'] = comRecord
+        #print comRecord
+
+
+        totalPlayer["W" + str(index/2 + 1)] = singlePlayer
+
+
+    # for keys, values in totalPlayer.items():
+    #     print keys, values
+    #     print "=" * 20
+
+    # print len(totalPlayer)
+
+    # print json.dumps(totalPlayer)
+    return totalPlayer
 
 def saveToFile(data):
 
@@ -112,8 +202,10 @@ if __name__=="__main__":
 
         add_into_done_list(key)
         data = getData(value)
-        #saveToFile(data)
-        parseHTML(data)
+
+        resultData = parseHTML(data)
+        save_total_record_json(resultData)
+
 
         print 'Here has queues =>', len(queues)
         print '-' * 20
